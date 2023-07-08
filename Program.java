26e3bae1be953e00428f88f161d4c31c5665be87
Jadson Mendes <jadsonmendes2018@gmail.com>
2023-07-08 14:18:08 -0300

    new changes

---
+++ Program.java
@@ -0,0 +1,6 @@
+public class Program{
+  public static void main(String[] args){
+    System.out.println("Rodando o sistema de gerenciamento de jogos");
+    System.out.println("A aplicação está se conectando ao banco de dados");
+  }
+}
