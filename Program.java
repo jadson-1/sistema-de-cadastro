682a18e959622c39b91228e27496ac003812bbe7
Jadson Mendes <jadsonmendes2018@gmail.com>
2023-07-10 17:34:03 -0300

    Retornando o sistema para o commit anterior

--- Program.java
+++ Program.java
@@ -3,6 +3,5 @@ public class Program{
     System.out.println("Rodando o sistema de gerenciamento de jogos");
     System.out.println("A aplicação está se conectando ao banco de dados");
     System.out.println("Teste");
-    System.out.println("Outro teste");
   }
 }
